f8c778bce065a49fa6a7af92f5383fdaa86846e5
indraxr6 <87898981+indraxr6@users.noreply.github.com>
2021-07-30 09:24:58 +0700

    Add files via upload

---
+++ Latihansoal2/src/latihansoal2/Latihansoal2.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihansoal2;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Latihansoal2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int a=5; a>=1; a--){
+            for (int i=1; i<=a; i++){
+            System.out.print("*");   
+        }
+        System.out.println(" ");
+    }
+    
+}
+}
+
+
+
+    
+    
+
